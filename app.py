858f048d191467ab009246f2273699dd0359aad0
Cecilia <c.rosales@berkeley.edu>
2026-08-30 00:04:07 -0700

    add Flask API with mentee, mentor, and match endpoints

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify
+from models import Mentee, Mentor
+from matcher import match_all
+
+app = Flask(__name__)
+mentees = []
+mentors = []
+
+@app.route("/mentees", methods=["GET"])
+def get_mentees():
+    return jsonify([m.to_dict() for m in mentees])
+
+@app.route("/mentees", methods=["POST"])
+def add_mentee():
+    data = request.get_json()
+    mentee = Mentee(data["id"], data["name"], data["major"], data["subjects"], data["skills"], data["interests"])
+    mentees.append(mentee)
+    return jsonify(mentee.to_dict()), 201
+
+@app.route("/mentors", methods=["GET"])
+def get_mentors():
+    return jsonify([m.to_dict() for m in mentors])
+
+@app.route("/mentors", methods=["POST"])
+def add_mentor():
+    data = request.get_json()
+    mentor = Mentor(data["id"], data["name"], data["major"], data["subjects"], data["skills"], data["interests"], data.get("capacity", 2))
+    mentors.append(mentor)
+    return jsonify(mentor.to_dict()), 201
+
+
+@app.route("/matches", methods=["GET"])
+def get_matches():
+    return jsonify(match_all(mentees, mentors))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- models.py
+++ models.py
@@ -6,6 +6,8 @@ class Mentee:
         self.subjects = subjects
         self.skills = skills
         self.interests = interests
+    def to_dict(self):
+        return {"id": self.id, "name": self.name, "major": self.major, "subjects":self.subjects, "skills":self.skills, "interests": self.interests}
 
 class Mentor:
     def __init__(self, id, name, major, subjects, skills, interests, capacity=2):
@@ -16,3 +18,5 @@ class Mentor:
         self.skills = skills
         self.interests = interests
         self.capacity = capacity
+    def to_dict(self):
+        return {"id": self.id, "name": self.name, "major": self.major, "subjects":self.subjects, "skills":self.skills, "interests": self.interests, "capacity":self.capacity}
